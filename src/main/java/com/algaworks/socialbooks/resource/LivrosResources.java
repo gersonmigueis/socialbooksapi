bb7f4150438a1f068135d55651518d32b20159e2
Gerson Migueis <gersonmigueis10@gmail.com>
2017-03-24 15:24:19 -0400

    subind readme

--- src/main/java/com/algaworks/socialbooks/resource/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resource/LivrosResources.java
@@ -1,14 +1,17 @@
 package com.algaworks.socialbooks.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
@@ -28,14 +31,27 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro){ //RequestBody : Pega o que esta na requisição e coloca no objeto livro
-		livrosRepository.save(livro);
+	public ResponseEntity<Void> salvar(@RequestBody Livro livro){ //RequestBody : Pega o que esta na requisição e coloca no objeto livro
+	    livro = livrosRepository.save(livro);
+		
+		URI uri  = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	//Buscar o livro por um determinado ID passado na URI: "/{id}" <- variavel
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public Livro buscar (@PathVariable Long id){ //Pego o valor na variavel acima 
-		return livrosRepository.findOne(id);
+	public ResponseEntity<?> buscar (@PathVariable Long id){ //Pego o valor na variavel acima 
+		//Trantando as respostas: ResponseEntity, notação <?> 
+		
+		Livro livro = livrosRepository.findOne(id);
+		
+		if (livro == null){
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
