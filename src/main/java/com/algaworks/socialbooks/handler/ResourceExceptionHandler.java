d079f3640df2d982cc0513aa693a276525554dab
Gerson Migueis <gersonmigueis10@gmail.com>
2017-05-23 14:00:54 -0400

    Upload

---
+++ src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.algaworks.socialbooks.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice // Permite que interceptemos todos os tipos de exceções que temos no nosso codigo
+public class ResourceExceptionHandler {
+
+}
