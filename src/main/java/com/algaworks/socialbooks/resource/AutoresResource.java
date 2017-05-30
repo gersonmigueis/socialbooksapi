839cd35f7fc294482b7a778db65f34edf6d513b5
Gerson Migueis <gersonmigueis10@gmail.com>
2017-05-30 17:20:36 -0400

    Tipos de Representação

--- src/main/java/com/algaworks/socialbooks/resource/AutoresResource.java
+++ src/main/java/com/algaworks/socialbooks/resource/AutoresResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +27,10 @@ public class AutoresResource {
 	private AutoresService autoresService;
 	
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE  //tipos de representação, json e xml 
+	})
 	public ResponseEntity<List<Autor>> listar(){
 		return ResponseEntity.status(HttpStatus.OK).body(autoresService.listar());
 	}
